6a8730d8330f6d4ca57f957422e13b08c8d36b4f
aosebih <aosebih@users.noreply.github.com>
2025-11-08 00:21:41 +0100

    'interface'

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI,Query
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import os
 app = FastAPI()
 
-@app.get("/")
+frontend_path = os.path.join(os.path.dirname(__file__), "frontend")
 
-def hi ():
-    return {"message" : "hello !"}
+app.mount("/static", StaticFiles(directory=os.path.join(frontend_path)), name="static")
+
+@app.get("/")
+async def root():
+    return FileResponse(os.path.join(frontend_path, "index.html"))
+
+@app.get("/calculate")
+
+def calculate(
+    w: float = Query(..., gt=0),
+    h: float = Query(..., gt=0)
+):
+    bmi = w / (h ** 2)
+    if bmi < 18.5:
+        category = "Underweight"
+    elif bmi < 25:
+        category = "Normal"
+    elif bmi < 30:
+        category = "Overweight"
+    else:
+        category = "Obese"
+    return {"bmi": round(bmi, 2), "category": category}
