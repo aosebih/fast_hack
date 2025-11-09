573a7d25b2248021839ff41bafa133b2604381e6
CoreyWKCO <coreywko@Corey.>
2025-11-09 22:17:30 +0100

    push test 1233

--- code.py
+++ code.py
@@ -2,12 +2,13 @@ w = float(input('enter your weight'))
 
 h = float(input('enter your height in cm'))
 
+#push test YR7EM WALDIK
 
 bmi = w /(h**2)
 
 if bmi > 24:
     print('do sport !')
 elif bmi < 18 :
-    print('go eat')
+    print('SPUSH TEYAA')
 else:
-    print('u r perfect' )
+    print('PUSH TEST' )
