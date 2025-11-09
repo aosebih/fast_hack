eb33a7a5a45b1b80981d81d7fb08321cc5758c78
CoreyWKCO <coreywko@Corey.>
2025-11-09 22:39:29 +0100

    push in omar

--- code.py
+++ code.py
@@ -1,14 +1,1 @@
-w = float(input('enter your weight'))
-
-h = float(input('enter your height in cm'))
-
-#push test YR7EM WALDIK
-
-bmi = w /(h**2)
-
-if bmi > 24:
-    print('do sport !')
-elif bmi < 18 :
-    print('SPUSH TEYAA')
-else:
-    print('PUSH TEST' )
+print('printed new branch omar')
